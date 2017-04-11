a13f6f78871b9d41e7236ed7a800e4f8e904ddd5
PusMinus0 <brandt.matthias+git@gmail.com>
2017-04-11 14:28:47 +0200

    Update formating

--- network.js
+++ network.js
@@ -1,30 +1,30 @@
 
 // create an array with nodes
 var nodes = new vis.DataSet([
-    {id: 'Anheuser-Busch InBev',            label: 'Anheuser-Busch InBev',            group: 'Anheuser-Busch InBev'},
-    {id: 'Astra',                           label: 'Astra',                           group: 'Carlsberg A/S'},
-    {id: 'Auerbräu',                        label: 'Auerbräu',                        group: 'Heineken N.V.'},
-    {id: 'Auerbräu GmbH',                  label: 'Auerbräu GmbH',                  group: 'Heineken N.V.'},
-    {id: 'Beck’s',                          label: 'Beck’s',                          group: 'Anheuser-Busch InBev'},
-    {id: 'Brau Holding International',      label: 'Brau Holding International',      group: 'Heineken N.V.'},
-    {id: 'Brauerei Beck GmbH & Co. KG',     label: 'Brauerei Beck GmbH & Co. KG',     group: 'Anheuser-Busch InBev'},
-    {id: 'Carlsberg',                       label: 'Carlsberg',                       group: 'Carlsberg A/S'},
-    {id: 'Carlsberg A/S',                   label: 'Carlsberg A/S',                   group: 'Carlsberg A/S'},
-    {id: 'Chiemseer',                       label: 'Chiemseer',                       group: 'Heineken N.V.'},
-    {id: 'Chiemgauer Brauhaus GmbH',        label: 'Chiemgauer Brauhaus GmbH',        group: 'Heineken N.V.'},
-    {id: 'Hacker-Pschorr',                  label: 'Hacker-Pschorr',                  group: 'Heineken N.V.'},
-    {id: 'Heineken N.V.',                   label: 'Heineken N.V.',                   group: 'Heineken N.V.'},
-    {id: 'Holsten',                         label: 'Holsten',                         group: 'Carlsberg A/S'},
-    {id: 'Holsten-Brauerei AG',             label: 'Holsten-Brauerei AG',             group: 'Carlsberg A/S'},
-    {id: 'Hopf',                            label: 'Hopf',                            group: 'Heineken N.V.'},
-    {id: 'Weißbierbrauerei Hopf GmbH',      label: 'Weißbierbrauerei Hopf GmbH',      group: 'Heineken N.V.'},
-    {id: 'Paulaner',                        label: 'Paulaner',                        group: 'Heineken N.V.'},
-    {id: 'Paulaner Brauerei GmbH & Co. KG', label: 'Paulaner Brauerei GmbH & Co. KG', group: 'Heineken N.V.'},
-    {id: 'Pfungstädter',                    label: 'Pfungstädter',                    group: 'Pfungstädter Brauerei Hildebrand GmbH & Co. KG'},
+    {id: 'Anheuser-Busch InBev',                           label: 'Anheuser-Busch InBev',                           group: 'Anheuser-Busch InBev'},
+    {id: 'Astra',                                          label: 'Astra',                                          group: 'Carlsberg A/S'},
+    {id: 'Auerbräu',                                       label: 'Auerbräu',                                       group: 'Heineken N.V.'},
+    {id: 'Auerbräu GmbH',                                  label: 'Auerbräu GmbH',                                  group: 'Heineken N.V.'},
+    {id: 'Beck’s',                                         label: 'Beck’s',                                         group: 'Anheuser-Busch InBev'},
+    {id: 'Brau Holding International',                     label: 'Brau Holding International',                     group: 'Heineken N.V.'},
+    {id: 'Brauerei Beck GmbH & Co. KG',                    label: 'Brauerei Beck GmbH & Co. KG',                    group: 'Anheuser-Busch InBev'},
+    {id: 'Carlsberg',                                      label: 'Carlsberg',                                      group: 'Carlsberg A/S'},
+    {id: 'Carlsberg A/S',                                  label: 'Carlsberg A/S',                                  group: 'Carlsberg A/S'},
+    {id: 'Chiemseer',                                      label: 'Chiemseer',                                      group: 'Heineken N.V.'},
+    {id: 'Chiemgauer Brauhaus GmbH',                       label: 'Chiemgauer Brauhaus GmbH',                       group: 'Heineken N.V.'},
+    {id: 'Hacker-Pschorr',                                 label: 'Hacker-Pschorr',                                 group: 'Heineken N.V.'},
+    {id: 'Heineken N.V.',                                  label: 'Heineken N.V.',                                  group: 'Heineken N.V.'},
+    {id: 'Holsten',                                        label: 'Holsten',                                        group: 'Carlsberg A/S'},
+    {id: 'Holsten-Brauerei AG',                            label: 'Holsten-Brauerei AG',                            group: 'Carlsberg A/S'},
+    {id: 'Hopf',                                           label: 'Hopf',                                           group: 'Heineken N.V.'},
+    {id: 'Weißbierbrauerei Hopf GmbH',                     label: 'Weißbierbrauerei Hopf GmbH',                     group: 'Heineken N.V.'},
+    {id: 'Paulaner',                                       label: 'Paulaner',                                       group: 'Heineken N.V.'},
+    {id: 'Paulaner Brauerei GmbH & Co. KG',                label: 'Paulaner Brauerei GmbH & Co. KG',                group: 'Heineken N.V.'},
+    {id: 'Pfungstädter',                                   label: 'Pfungstädter',                                   group: 'Pfungstädter Brauerei Hildebrand GmbH & Co. KG'},
     {id: 'Pfungstädter Brauerei Hildebrand GmbH & Co. KG', label: 'Pfungstädter Brauerei Hildebrand GmbH & Co. KG', group: 'Pfungstädter Brauerei Hildebrand GmbH & Co. KG'},                     
-    {id: 'Schörghuber Unternehmensgruppe',  label: 'Schörghuber Unternehmensgruppe'},
-    {id: 'Thurn und Taxis',                 label: 'Thurn und Taxis',                 group: 'Heineken N.V.'},
-    {id: 'Weldebräu',                       label: 'Weldebräu',                       group: 'Weldebräu'},
+    {id: 'Schörghuber Unternehmensgruppe',                 label: 'Schörghuber Unternehmensgruppe'},
+    {id: 'Thurn und Taxis',                                label: 'Thurn und Taxis',                                group: 'Heineken N.V.'},
+    {id: 'Weldebräu',                                      label: 'Weldebräu',                                      group: 'Weldebräu'}
 ]);
 
 // create an array with edges
@@ -48,7 +48,7 @@ var edges = new vis.DataSet([
     {from: 'Paulaner Brauerei GmbH & Co. KG', to: 'Brau Holding International'},
     {from: 'Paulaner Brauerei GmbH & Co. KG', to: 'Schörghuber Unternehmensgruppe'},
     {from: 'Pfungstädter', to: 'Pfungstädter Brauerei Hildebrand GmbH & Co. KG'},
-    {from: 'Thurn und Taxis', to: 'Paulaner Brauerei GmbH & Co. KG'},
+    {from: 'Thurn und Taxis', to: 'Paulaner Brauerei GmbH & Co. KG'}
 ]);
 
 // provide the data in the vis format
